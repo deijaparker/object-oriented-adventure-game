f9fa78a73ae581de29fe479a087c591f9f2d9525
deijaparker <deijaparker1@outlook.com>
2024-08-13 15:38:59 -0700

    Add initial adventurer object with nested objects and methods

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Part 1: Humble Beginnings
+
+// Initial adventurer object with nested objects and methods
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+      name: "Leo",
+      type: "Cat",
+      companion: {
+        name: "Frank",
+        type: "Flea",
+        inventory: ["small hat", "sunglasses"]
+      }
+    },
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    }
+  };
